4ead8bf3326de39ae2846f55589033839d50ddee
zablon <zablonoigo51@gmail.com>
2024-07-21 22:46:44 +0300

    defining variable

--- RDS/aurora/infra/variables.tf
+++ RDS/aurora/infra/variables.tf
@@ -1,1 +1,9 @@
- 
+variable "access_key" {
+    description = "Access key to AWS console"
+}
+variable "secret_key" {
+    description = "Secret key to AWS console"
+}
+variable "region" {
+    description = "AWS region"
+} 
