5965e2c68d528b546d9c12b89e91b06e3cbafe68
zablon <zablonoigo51@gmail.com>
2024-07-21 22:48:57 +0300

    creating a subnet group

--- RDS/aurora/infra/main.tf
+++ RDS/aurora/infra/main.tf
@@ -4,3 +4,26 @@ provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
 }
+# Creating DB Subnet Group
+resource "aws_db_subnet_group" "mydb_subnet_group" {
+  name       = "mydb-subnet-group"
+  subnet_ids = [
+                data.aws_subnet.subnet1.id,
+                data.aws_subnet.subnet2.id
+                ]
+  tags = {
+    Name = "MyDBSubnetGroup"
+  }
+}
+  resource "aws_rds_cluster" "aurorards" {
+    cluster_identifier      = "myauroracluster"
+    engine                  = "aurora-mysql"
+    engine_version          = "5.7.mysql_aurora.2.12.0"
+    database_name           = "MyDB"
+    master_username         = "Admin"
+    master_password         = "Test123"
+    vpc_security_group_ids = [aws_security_group.allow_aurora.id]
+    db_subnet_group_name  = aws_db_subnet_group.mydb_subnet_group.name
+    storage_encrypted = false
+    skip_final_snapshot   = true       
+}
