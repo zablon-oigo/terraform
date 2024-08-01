cd36d3f8a67c495fe472847e99c534098b0890f2
zablon <zablonoigo51@gmail.com>
2024-08-01 09:02:21 +0300

    Creating a security group for EC2

--- NetworkLoadBalancer/infra/main.tf
+++ NetworkLoadBalancer/infra/main.tf
@@ -17,3 +17,42 @@ data "aws_subnet" "subnet2" {
     vpc_id = data.aws_vpc.vpc.id
     availability_zone = "us-east-1b"
 }
+# Creating Security Group for EC2
+resource "aws_security_group" "ec2_sg" {
+    name        = "NLBserver-SG"
+    description = "Security Group to allow traffic to EC2"
+    vpc_id = data.aws_vpc.vpc.id
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+# Creating Key pair for EC2 
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "my_key" {
+  key_name   = "myKey"
+  public_key = tls_private_key.key.public_key_openssh
+}
