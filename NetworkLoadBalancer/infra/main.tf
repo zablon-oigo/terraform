04bd02386c95435a297642bfdff6b957c9c111ec
zablon <zablonoigo51@gmail.com>
2024-08-01 09:00:27 +0300

    Adding default VPC

--- NetworkLoadBalancer/infra/main.tf
+++ NetworkLoadBalancer/infra/main.tf
@@ -3,3 +3,17 @@ provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
 }			
+# Default VPC 
+data "aws_vpc" "vpc" {
+    default = true
+}
+
+data "aws_subnet" "subnet1" {
+    vpc_id = data.aws_vpc.vpc.id
+    availability_zone = "us-east-1a"
+}
+
+data "aws_subnet" "subnet2" {
+    vpc_id = data.aws_vpc.vpc.id
+    availability_zone = "us-east-1b"
+}
