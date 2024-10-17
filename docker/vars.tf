4031476c046ee0f8243e76e7831471a023c2e0d2
zablon <zablonoigo51@gmail.com>
2024-10-17 19:55:34 +0300

    define variables

---
+++ docker/vars.tf
@@ -0,0 +1,12 @@
+variable "secret_key" {
+description = "secret key"
+}
+variable "access_key" {
+  description = "access key"
+}
+variable "token"{
+    description = "default session token"
+}
+variable "region" {
+  description = "default region"
+}
