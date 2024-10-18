d18177ad3bd36a4bb0ccb4400754762820bdabe3
zablon <zablonoigo51@gmail.com>
2024-10-18 06:45:39 +0300

    tls keys config

--- docker/main.tf
+++ docker/main.tf
@@ -4,3 +4,12 @@ provider "aws" {
     access_key = "${var.access_key}"
     token="${var.token}"
 }
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "test"
+  public_key = tls_private_key.key.public_key_openssh
+}
