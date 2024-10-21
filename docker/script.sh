f9aedea784f826657bcd795e3ab04cf9e50a357b
zablon <zablonoigo51@gmail.com>
2024-10-21 18:33:31 +0300

    Add script file

---
+++ docker/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+usermod -aG docker ubuntu
+sudo docker pull zablondev/webserver:latest
+sudo docker run --name web --rm -d -p 80:80 zablondev/webserver
