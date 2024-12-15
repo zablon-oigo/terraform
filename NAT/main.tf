480dcd3ca3367eb637f22c811ada98d66f713758
zablon <zablonoigo51@gmail.com>
2024-12-15 20:33:04 +0300

    Add custom vpc

--- NAT/main.tf
+++ NAT/main.tf
@@ -3,3 +3,9 @@ provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
 }
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyVPC"
+  }
+}			
