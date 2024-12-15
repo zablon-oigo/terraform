9d5bb445725c2dcd0c23313ec8002fd68e3630e5
zablon <zablonoigo51@gmail.com>
2024-12-15 20:34:01 +0300

    Create a public subnet

--- NAT/main.tf
+++ NAT/main.tf
@@ -9,3 +9,11 @@ resource "aws_vpc" "vpc" {
     Name = "MyVPC"
   }
 }			
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+   tags = {
+    Name = "MyPublicSubnet"
+  }
+}
