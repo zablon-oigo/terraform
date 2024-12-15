b46a7d8f0048489f27ca21989958dbc976927dfd
zablon <zablonoigo51@gmail.com>
2024-12-15 20:34:32 +0300

    Create a private subnet

--- NAT/main.tf
+++ NAT/main.tf
@@ -17,3 +17,10 @@ resource "aws_subnet" "subnet1" {
     Name = "MyPublicSubnet"
   }
 }
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+   tags = {
+    Name = "MyPrivateSubnet"
+  }
+}			
