768fbc6f7a78c5d4bb8d8b91c19f60c6f91c591c
zablon <zablonoigo51@gmail.com>
2024-12-16 21:14:22 +0300

    Associate route with public subnet

--- NAT/main.tf
+++ NAT/main.tf
@@ -38,5 +38,7 @@ resource "aws_route_table" "routetable" {
     gateway_id = aws_internet_gateway.igw.id
   }
 }
-	
-
+resource "aws_route_table_association" "subnetassociation" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.routetable.id
+}		
