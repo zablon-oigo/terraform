e0cfc2d971702fee65c1e76dbe99ee72191ff253
zablon <zablonoigo51@gmail.com>
2024-12-16 21:13:31 +0300

    Create public route table

--- NAT/main.tf
+++ NAT/main.tf
@@ -31,3 +31,12 @@ resource "aws_internet_gateway" "igw" {
   }
 }			
 
+resource "aws_route_table" "routetable" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+	
+
