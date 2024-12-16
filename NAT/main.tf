9380b76fc1962268a699ad255765e8da75d410e6
zablon <zablonoigo51@gmail.com>
2024-12-16 21:18:10 +0300

    Allow outgoing traffic

--- NAT/main.tf
+++ NAT/main.tf
@@ -53,4 +53,13 @@ resource "aws_security_group" "ec2sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "test_sg"
+  }
 }			
