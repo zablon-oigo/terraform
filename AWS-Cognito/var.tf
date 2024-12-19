6ca6d124ca3ac56efa3d0d42d88b3beaa3b313e6
zablon <zablonoigo51@gmail.com>
2024-12-19 17:20:16 +0300

    Add variable

--- AWS-Cognito/var.tf
+++ AWS-Cognito/var.tf
@@ -1,3 +1,9 @@
-region = "us-west-2"
-access_key = ""        
-secret_key = ""
+variable "access_key" {
+    description = ""
+}
+variable "secret_key" {
+    description = "Secret key"
+}
+variable "region" {
+    description = "region"
+}
