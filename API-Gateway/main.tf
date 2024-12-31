8ccd6761e4f0896c3b9aa39956055ac786b051b8
zablon <zablonoigo51@gmail.com>
2024-12-31 22:52:01 +0300

    Add a response for the get method

--- API-Gateway/main.tf
+++ API-Gateway/main.tf
@@ -29,3 +29,12 @@ resource "aws_api_gateway_method" "testMethod" {
     http_method   = "GET"
     authorization = "NONE"           
 }
+resource "aws_api_gateway_method_response" "response_200" {
+    rest_api_id = aws_api_gateway_rest_api.testAPI.id
+    resource_id = aws_api_gateway_resource.testresource.id
+    http_method = aws_api_gateway_method.testMethod.http_method
+    status_code = "200"
+    response_models = {
+         "application/json" = "Empty"        
+     }           
+}
