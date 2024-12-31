066538dc1211c08cc496b4f9a13237533299725b
zablon <zablonoigo51@gmail.com>
2024-12-31 22:46:47 +0300

    create a lambda function

--- API-Gateway/main.tf
+++ API-Gateway/main.tf
@@ -3,3 +3,12 @@ provider "aws"{
     secret_key=""
     access_key = ""
 }
+
+resource "aws_lambda_function" "test_lambda" {
+    filename         = "lambda_function.zip"
+    function_name    = "test"
+    role             = "<ARN>"
+    handler          = "lambda_function.lambda_handler"
+    runtime          = "python3.9"
+    source_code_hash = filebase64sha256("lambda_function.zip")
+}
