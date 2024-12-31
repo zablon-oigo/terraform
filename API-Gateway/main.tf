b4963e18415279226c27882b53dfac143409533c
zablon <zablonoigo51@gmail.com>
2024-12-31 22:55:07 +0300

    Add a response for the integration

--- API-Gateway/main.tf
+++ API-Gateway/main.tf
@@ -47,3 +47,12 @@ resource "aws_api_gateway_integration" "MyDemoIntegration" {
     type                 = "AWS"
     passthrough_behavior    = "WHEN_NO_TEMPLATES"           
 }  
+
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+    rest_api_id        = aws_api_gateway_rest_api.testAPI.id
+    resource_id        = aws_api_gateway_resource.testresource.id
+    http_method        = aws_api_gateway_method.testMethod.http_method
+    status_code        = aws_api_gateway_method_response.response_200.status_code
+    response_templates = { "application/json" = "" }
+    depends_on         = [aws_api_gateway_integration.MyDemoIntegration]
+}
