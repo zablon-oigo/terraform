370e0559d087b2d4e65c6aac0264c6a2d75380bc
zablon <zablonoigo51@gmail.com>
2024-12-31 22:54:02 +0300

    Add api gateway integration and deploy thr api

--- API-Gateway/main.tf
+++ API-Gateway/main.tf
@@ -38,3 +38,12 @@ resource "aws_api_gateway_method_response" "response_200" {
          "application/json" = "Empty"        
      }           
 }
+resource "aws_api_gateway_integration" "MyDemoIntegration" {
+    rest_api_id          = aws_api_gateway_rest_api.testAPI.id
+    resource_id          = aws_api_gateway_resource.testresource.id
+    http_method          = aws_api_gateway_method.testMethod.http_method
+    integration_http_method     = "POST"
+     uri = aws_lambda_function.test_lambda.invoke_arn
+    type                 = "AWS"
+    passthrough_behavior    = "WHEN_NO_TEMPLATES"           
+}  
