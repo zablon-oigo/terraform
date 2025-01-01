69c3a445060440c1c5e02f809db4784bf5ba5c5e
zablon <zablonoigo51@gmail.com>
2025-01-01 14:03:51 +0300

    fetch default vpc and a subnet

--- API-Gateway-DynamoDB/main.tf
+++ API-Gateway-DynamoDB/main.tf
@@ -3,3 +3,11 @@ provider "aws"{
     access_key = var.access_key
     secret_key = var.secret_key
 }
+data "aws_vpc" "default"{
+    default = true
+}
+data "aws_subnet" "subnet" {
+    vpc_id = data.aws_vpc.default.id
+    availability_zone = "us-east-1a"
+  
+}
